e8a107ef658239b3f019536a76ad9efb756e010f
choko <ro22113176@gmail.com>
2021-05-03 16:59:10 +0800

    state uplift

--- src/Main.js
+++ src/Main.js
@@ -4,10 +4,25 @@ import User from './User.js';
 
 class Main extends React.Component{
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            dialog:[],
+        }
+    }
+
+    handleChatSubmit(msg){
+        this.setState({
+            dialog: this.state.dialog.concat(msg),
+        })
+    }
+
+
     render(){
         return (
-          <div className="Main">
-            <User />
+          <div className = "Main">
+            <User onSubmit={() => this.handleChatSubmit()}/>
           </div>
         );
     }
--- src/User.js
+++ src/User.js
@@ -2,34 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class User extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            value: '',
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
         event.preventDefault();
-    }
+        this.props.onSubmit();
 
+    }
 
     render() {
         return (
-            <form onSubmit ={this.handleSubmit}>
+            <form onSubmit ={this.props.onSubmit()}>
                 <label > do you want to talk to me ?
                     <p>
-                        <input type="text" value={this.state.value} onChange={this.handleChange} />
-                        <input type='submit' value="Submit" />
+                        <input type="text" />
+                        <input type='submit' value = "Send"/>
                     </p>
                 </label>
 
