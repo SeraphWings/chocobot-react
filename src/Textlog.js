42ee7cac28e0009eb416b7cd4d0544b34e41a118
choko <ro22113176@gmail.com>
2021-05-04 13:20:04 +0800

    able to response

--- src/Textlog.js
+++ src/Textlog.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Textlog extends React.Component {
-    render(){
-        return(
-            const logs = this.props.dialog.map( (element) => {
-                <li>{element}</li>
-            }
 
+
+class Textlog extends React.Component {
+
+    generate(dialog) {
+
+
+        const logs = dialog.map( (element, index) => {
+             return (
+                <li key={index}>
+                    {element}
+                </li>
+             )
+        });
+        // console.log(dialog);
+        // console.log(logs);
+        return (
+            <ul>{logs}</ul>
+        );
+    };
+
+    render() {
+        return (
+            <div>{this.generate(this.props.dialog)}</div>
         )
-    }
+
+    };
 }
 
 export default Textlog;
